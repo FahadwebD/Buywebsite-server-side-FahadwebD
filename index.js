79e3ddb0d5e2e351ede5ca61d81655e8b80b9777
FahadwebD <fahadchowdhury66779@gmail.com>
2021-11-12 04:09:05 +0600

    order

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ffrgt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -16,8 +17,23 @@ async function run (){
 
     try{
         await client.connect();
-        console.log('connectdet')
+        const database = client.db('buy_watch');
+        const productCollection = database.collection('products');
+        const ordersCollection = database.collection('orders');
 
+        app.get('/products' , async(req , res)=>{
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+         
+
+        app.post('/orders' , async(req ,res)=>{
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order)
+            console.log(order);
+            res.json(result)
+        })
     }
     finally{
         // await client.close();
